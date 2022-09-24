2978f12d98a85c7904ff18efbadd18a5735d714a
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-09-24 06:40:23 +0200

    added db setup function

--- db_setup.py
+++ db_setup.py
@@ -2,39 +2,46 @@ import shared
 import zipfile
 import os
 
-# create intial CIM900 data table
-sql_db = shared.create_connection("cim900.db")
-shared.create_table(
-    sql_db,
-    "CREATE TABLE IF NOT EXISTS cim900_data (_data text);",
-)
 
-# create view for branches
-shared.create_table(
-    sql_db,
-    "CREATE VIEW IF NOT EXISTS view_branch_details AS select distinct trim(substr(_data,3,6)) AS branch_no,trim(substr(_data,9,30)) AS branch_name,(case when (trim(substr(_data,9,30)) like '%closed%') then 'Y' else 'N' end) AS Closed,trim(substr(_data,39,30)) AS member_name,trim(substr(_data,77,6)) AS member_no,trim(substr(_data,273,35)) AS country from cim900_data;",
-)
+def setup_CDV():
+    # create intial CIM900 data table
+    sql_db = shared.create_connection("cim900.db")
+    shared.create_table(
+        sql_db,
+        "CREATE TABLE IF NOT EXISTS cim900_data (_data text);",
+    )
 
-# extract CIM900.zip
-with zipfile.ZipFile("CIM900.zip", "r") as zip_ref:
-    zip_ref.extractall(".")
+    # create view for branches
+    shared.create_table(
+        sql_db,
+        "CREATE VIEW IF NOT EXISTS view_branch_details AS select distinct trim(substr(_data,3,6)) AS branch_no,trim(substr(_data,9,30)) AS branch_name,(case when (trim(substr(_data,9,30)) like '%closed%') then 'Y' else 'N' end) AS Closed,trim(substr(_data,39,30)) AS member_name,trim(substr(_data,77,6)) AS member_no,trim(substr(_data,273,35)) AS country from cim900_data;",
+    )
 
-# read CIM900 file
-with open("CIM900", "r") as f:
-    data = f.readlines()
+    # extract CIM900.zip
+    with zipfile.ZipFile("CIM900.zip", "r") as zip_ref:
+        zip_ref.extractall(".")
 
-# remove file
-os.remove("CIM900")
+    # read CIM900 file
+    with open("CIM900", "r") as f:
+        data = f.readlines()
 
-# insert data into table
-shared.insert_data_many(
-    sql_db,
-    "INSERT OR IGNORE INTO cim900_data VALUES (?);",
-    [(x,) for x in data],
-)
+    # remove file
+    os.remove("CIM900")
+
+    # insert data into table
+    shared.insert_data_many(
+        sql_db,
+        "INSERT OR IGNORE INTO cim900_data VALUES (?);",
+        [(x,) for x in data],
+    )
+
+    return sql_db
+
 
 if __name__ == "__main__":
     import pprint
+
+    sql_db = setup_CDV()
 
     pprint.pprint(
         shared.select_data(sql_db, "SELECT * FROM view_branch_details limit 5;")
