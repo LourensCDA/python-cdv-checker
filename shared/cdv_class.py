c0521e9e386afad5e6a864183ceee177a0d860a9
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-09-24 15:21:39 +0200

    updated pre cdv checks

--- shared/cdv_class.py
+++ shared/cdv_class.py
@@ -63,11 +63,25 @@ class cdv_class:
     def standard_bank_checks(self, branch_code):
         outcome = None
         if (
-            self.banking_details["account_number_length"] == 11
+            branch_code != "000000"
+            and self.banking_details["account_number_length"] == 11
             and self.banking_details["account_number"][:1] != "0"
             and self.banking_details["branch_code"] != branch_code
         ):
             return {"success": False, "message": "Account number must start with 0"}
+        if (
+            branch_code == "051001"
+            and self.banking_details["account_number_length"] == 11
+            and self.banking_details["account_number"][:1] != "1"
+            and self.banking_details["branch_code"] == branch_code
+        ):
+            return {"success": False, "message": "Account number must start with 1"}
+        if (
+            branch_code == "000000"
+            and self.banking_details["account_number_length"] == 13
+            and self.banking_details["account_number"][3] not in (2, 4)
+        ):
+            return {"success": False, "message": "Account number invalid"}
         return outcome
 
     ## return cdv pass or fail
@@ -94,13 +108,51 @@ class cdv_class:
         if not self.banking_details["account_number"].isnumeric():
             return {"success": False, "message": "Account number not numeric"}
         # standard bank checks
-        if branch["member_name"].lower() == "standard bank":
-            output = (
-                self.standard_bank_checks("051001")
-                if branch["country"].lower() == "south africa"
-                else self.standard_bank_checks("087373")
-            )
+        if (
+            branch["member_name"].lower() == "standard bank"
+            or self.banking_details["branch_code"] == "051001"
+        ):
+            if (
+                branch["country"].lower() == "south africa"
+                or self.banking_details["branch_code"] == "051001"
+            ):
+                output = self.standard_bank_checks("051001")
+            elif branch["country"].lower() == "namibia":
+                output = self.standard_bank_checks("087373")
+            elif branch["member_name"].upper() in (
+                "STANDARD BANK SWAZILAND",
+                "STANDARD LESOTHO BANK",
+                "STANDARD LESOTHO BANK LTD",
+            ):
+                output = self.standard_bank_checks("000000")
             if output:
                 return output
-
+        # check for branch code 087373
+        if (
+            self.banking_details["branch_code"] == "087373"
+            and self.banking_details["account_number_length"] == 11
+            and self.banking_details["account_number"][:1] != "6"
+        ):
+            return {"success": False, "message": "Account number must start with 6"}
+        # check for habib bank
+        if (
+            self.banking_details["member_name"].upper() == "HABIB OVERSEAS BANK LIMITED"
+            and self.banking_details["account_number_length"] == 11
+            and self.banking_details["account_number"][:1] != "0"
+        ):
+            return {"success": False, "message": "Account number must start with 0"}
+        # get weighting factor
+        self.banking_details["account_number_fmt"] = (
+            ("00000000000" + self.banking_details["account_number"])[-11:]
+            if self.banking_details["account_number_length"] < 11
+            else self.banking_details["account_number"]
+        )
+        # get weighting factor
+        wf = sqlite_select_data(
+            self.db_file,
+            f"""SELECT * FROM view_weighting_factor WHERE branch_no = '{self.banking_details["branch_code"]}' and CDVAccType = '{self.banking_details["account_type_no"]}'""",
+        )[0]
+        # if no weighting factor return default
+        if not wf or wf["EFT_CDV_AccInd"] == 0:
+            return output
         return output
